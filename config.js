3db59da74d8b1bc5756afd50d083ccd6c104c14f
gilzzbotz <104905739+gilzzbotz@users.noreply.github.com>
2022-11-07 19:29:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -371,8 +371,8 @@ global.hwaifu = ['https://i.pinimg.com/originals/ed/34/f8/ed34f88af161e6278993e1
 'https://i.pinimg.com/originals/fd/21/41/fd21419275236bb153de3c8dcbbf3bf9.jpg',
 'https://i.pinimg.com/originals/80/4f/1a/804f1a05f9996c96a2d492b4854b7fd5.jpg']
 /*============== STICKER WM ==============*/
-global.stickpack = 'ᴍᴀᴅᴇ ʙʏ'
-global.stickauth = `King Of Bear`
+global.stickpack = 'Sewa Bot?'
+global.stickauth = `wa.me/6285730794089`
 
 global.multiplier = 38 // The higher, The harder levelup
 
